114baace47485502b100cb36605e66d7b5aea2ea
roger20415 <roger20415@gmail.com>
2025-03-28 19:49:39 +0800

    [FEAT] Remove data pub sub time for Unity.

--- rl_training_pipeline/subscribe_data/data_subscribe.py
+++ rl_training_pipeline/subscribe_data/data_subscribe.py
@@ -1,5 +1,4 @@
 import sys
-from datetime import datetime
 
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
@@ -22,21 +21,5 @@ class DataSubscriberNode(Node):
         )
 
     def _state_subscribe_callback(self, msg: Float32MultiArray) -> None:
-        now = datetime.now()
-        now_seconds = now.second + now.microsecond / 1_000_000
         self._unity_data_store.split_and_store_received_array(msg)
         sys.stderr.write(f"\033[35m{msg.data}\033[0m\n")
-        self._print_pub_sub_info(
-            msg.data[2],
-            msg.data[3],
-            now_seconds
-        )
-        self._last_pub_time = msg.data[3]
-
-
-    def _print_pub_sub_info(self, fixupdate_count: float, pub_time: float, sub_time: float) -> None:
-        sys.stderr.write("\033[92mState: {:.0f}\033[0m\n".format(fixupdate_count))
-        sys.stderr.write("\033[92mPub time: {:.4f}s\033[0m\n".format(pub_time))
-        sys.stderr.write("\033[32mSub time: {:.4f}s\033[0m\n".format(sub_time))
-        sys.stderr.write("\033[92m2 Pub Interval: {:.4f}s\033[0m\n".format(pub_time - self._last_pub_time))
-        sys.stderr.write("\033[92mPub-Sub Interval: {:.4f}s\n\033[0m\n".format(sub_time - pub_time))
